3dff6446b6009964de405a806ef4ceab60f04c90
Artur <artur.sharafutdinov101@yandex.ru>
2020-11-03 10:45:48 +0300

    Create project and add first CRUD for table Books

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProj.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime DateIn { get; set; }
+
+        public string Info { get; set; }
+
+        public bool isHave { get; set; }
+
+        public string Link { get; set; }
+    }
+}
---
+++ Models/BooksContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProj.Models
+{
+    public class BooksContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+
+        public BooksContext(DbContextOptions<BooksContext> options)
+         : base(options)
+        {
+        }
+
+    }
+}
